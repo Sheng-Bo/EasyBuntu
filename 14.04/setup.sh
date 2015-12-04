c8e2e513c92f7c565c2855b59593aa56c7c5f3dc
Sheng-Bo Chen <artalose@gmail.com>
2015-12-04 13:20:45 -0500

    1.1.0

--- 14.04/setup.sh
+++ 14.04/setup.sh
@@ -23,3 +23,36 @@ sudo apt-get install synaptic -y
 sudo add-apt-repository ppa:tualatrix/ppa -y
 sudo apt-get update
 sudo apt-get install ubuntu-tweak -y
+
+# 安裝 Compiz 設置工具，可以關閉視窗動畫
+sudo apt-get install compizconfig-settings-manager -y
+
+# 安裝 Gparted 磁碟分割工具
+sudo apt-get install gparted -y
+
+# 安裝字型管理員
+sudo apt-get install font-manager -y
+
+# 安裝 Webmin
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+curl -O -L http://prdownloads.sourceforge.net/webadmin/webmin_1.770_all.deb
+sudo dpkg -i webmin_1.770_all.deb
+sudo apt-get -f install -y
+
+
+# ============================================================
+# 網路應用
+# ============================================================
+
+# 安裝 Google Chrome 瀏覽器
+curl -O -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# 安裝 Skype
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" -y
+sudo apt-get update
+sudo apt-get install skype -y
+sudo apt-get -f install -y
+
+# 安裝 Filezilla
+sudo apt-get install filezilla -y

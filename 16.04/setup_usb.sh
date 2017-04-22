93ffb81f266914e6958bba4a204e6c5d9ab1b3ac
Sheng-Bo Chen <artalose@gmail.com>
2017-04-22 17:52:51 +0000

    Update setup_usb.sh

--- 16.04/setup_usb.sh
+++ 16.04/setup_usb.sh
@@ -15,25 +15,28 @@ curl -O -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install -y
 
-# Fonts
+# Microsoft Fonts
 wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
 sudo apt-get purge ttf-mscorefonts-installer -y
 sudo apt install ./ttf-mscorefonts-installer_3.6_all.deb
 sudo dpkg-reconfigure ttf-mscorefonts-installer
 
-#install fonts
-sudo apt-get install texlive-fonts-recommended texlive-fonts-extra
+# Latex fonts
+sudo apt-get install texlive-fonts-recommended texlive-fonts-extra -y
 
-#install apa6/biblatex stuff
-sudo apt-get install biblatex biblatex-dw
+# APA 6
+#sudo apt-get install biblatex biblatex-dw
 sudo apt-get install texlive-publishers
-
 sudo apt-get install texlive-bibtex-extra
 sudo texhash
-
 
 # Lyx
 sudo add-apt-repository ppa:lyx-devel/release
 sudo apt-get update
 sudo apt-get install lyx
+
+# WPS Office
+curl -O -L http://kdl.cc.ksosoft.com/wps-community/download/a19/wps-office_9.1.0.4975~a19p1_amd64.deb
+sudo dpkg -i wps-office_9.1.0.4975~a19p1_amd64.deb
+
 

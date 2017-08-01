17e8dbd0dd91368432ab0d2684b287a18bc85e26
Sheng-Bo Chen <artalose@gmail.com>
2017-08-01 14:00:35 -0400

    Update setup2.sh

--- 16.04/setup2.sh
+++ 16.04/setup2.sh
@@ -2,3 +2,6 @@ wget -O mendeleydesktop.deb https://www.mendeley.com/repositories/ubuntu/stable/
 sudo dpkg -i mendeleydesktop.deb
 sudo apt install -f
 
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+sudo dpkg -i skypeforlinux-64.deb
+sudo apt install -f

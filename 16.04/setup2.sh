a2cc8008cf9e5b9ff77243956036739e46b7e7a1
Sheng-Bo Chen <artalose@gmail.com>
2017-08-01 14:09:24 -0400

    Update setup2.sh

--- 16.04/setup2.sh
+++ 16.04/setup2.sh
@@ -5,3 +5,7 @@ sudo apt install -f
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 sudo dpkg -i skypeforlinux-64.deb
 sudo apt install -f
+
+http://download3.operacdn.com/pub/opera/desktop/46.0.2597.57/linux/opera-stable_46.0.2597.57_amd64.deb
+sudo dpkg -i skypeforlinux-64.deb
+sudo apt install -f

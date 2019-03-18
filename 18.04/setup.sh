ee3c7cf8657759387255f90c71721f25fcdba8c8
Sheng-Bo Chen <artalose@gmail.com>
2019-03-18 19:53:35 -0400

    Update setup.sh

--- 18.04/setup.sh
+++ 18.04/setup.sh
@@ -21,6 +21,8 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 
+#JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+
 
 
 # ============================================================
